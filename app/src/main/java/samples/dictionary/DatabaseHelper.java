c65dd8f3a905d59c9a4e495e158c125014d4005b
Stefan Cizmar <stefan.cizmar.pp@gmail.com>
2021-03-22 19:58:08 +0100

    Reformat code

--- app/src/main/java/samples/dictionary/DatabaseHelper.java
+++ app/src/main/java/samples/dictionary/DatabaseHelper.java
@@ -21,9 +21,9 @@ import kiwi.employeedirectory.R;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-	public static final String DATABASE_NAME = "employee";
-	public static final String TABLE_NAME="employee";
-	
+    public static final String DATABASE_NAME = "employee";
+    public static final String TABLE_NAME = "employee";
+
     public static final String ID = "_id";
     public static final String WORD = "word";
     public static final String ARTH = "arth";
@@ -33,67 +33,67 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String REMARK = "remark";
 
-	public static final String REMARKSK = "remarkSk";
-	public static final String REMARKEL = "remarkEl";
-	//public static final String DEFINITION2 = "definition2";
+    public static final String REMARKSK = "remarkSk";
+    public static final String REMARKEL = "remarkEl";
+    //public static final String DEFINITION2 = "definition2";
 
-	public static final String DEFINITIONSK = "definitionSk";
-	public static final String DEFINITIONEL = "definitionEl";
+    public static final String DEFINITIONSK = "definitionSk";
+    public static final String DEFINITIONEL = "definitionEl";
 
-	public static final String FAVORITES = "favorites";
+    public static final String FAVORITES = "favorites";
     //public static final String NOFRASES = "nofrases";
     public static final String DECLENSION = "declension";
 
-	protected Context context;
+    protected Context context;
 
-	public DatabaseHelper(Context context) {
-		
-		super(context, DATABASE_NAME, null, 16); // number  is version db
-		this.context = context;
-	}
+    public DatabaseHelper(Context context) {
 
-	@Override
-	public void onCreate(final SQLiteDatabase db) {
-	String s;
+        super(context, DATABASE_NAME, null, 16); // number  is version db
+        this.context = context;
+    }
 
-		try {
-	        
-	        Log.d("Helper", "Create database.");
-			InputStream in = context.getResources().openRawResource(R.raw.sql07122020);
-			DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-			Document doc = builder.parse(in, null);
-			NodeList statements = doc.getElementsByTagName("statement");
-			for (int i=0; i<statements.getLength(); i++) {
-				s = statements.item(i).getChildNodes().item(0).getNodeValue();
-				db.execSQL(s);
-				
-			}
-		} catch (Throwable t) {
+    @Override
+    public void onCreate(final SQLiteDatabase db) {
+        String s;
 
-		        //Toast.makeText(context, t.toString(), Toast.LENGTH_LONG).show();
-		        Log.d("Helper", t.toString());
-		        
-			}
-		
-		
-		Log.d("Helper", "Database done.");
-		Toast.makeText(context, R.string.create_database_done, Toast.LENGTH_LONG).show();
-	}
+        try {
 
-	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		
-		db.execSQL("DROP TABLE IF EXISTS employee");
-		onCreate(db);
-	}
-	
-	
-	Handler handle = new Handler() {
+            Log.d("Helper", "Create database.");
+            InputStream in = context.getResources().openRawResource(R.raw.sql07122020);
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document doc = builder.parse(in, null);
+            NodeList statements = doc.getElementsByTagName("statement");
+            for (int i = 0; i < statements.getLength(); i++) {
+                s = statements.item(i).getChildNodes().item(0).getNodeValue();
+                db.execSQL(s);
+
+            }
+        } catch (Throwable t) {
+
+            //Toast.makeText(context, t.toString(), Toast.LENGTH_LONG).show();
+            Log.d("Helper", t.toString());
+
+        }
+
+
+        Log.d("Helper", "Database done.");
+        Toast.makeText(context, R.string.create_database_done, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("DROP TABLE IF EXISTS employee");
+        onCreate(db);
+    }
+
+
+    Handler handle = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
         }
     };
 
-    
+
 }
